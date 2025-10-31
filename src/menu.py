10cc6adf78c14600081811a7863e8b6b5c8c604b
Unoxya <leonardoc.melo2002@gmail.com>
2025-10-31 09:06:37 -0300

    Criação do Menu de Estatisticas e refatoramento do módulo

--- src/menu.py
+++ src/menu.py
@@ -1,47 +1,131 @@
+def print_header(title: str, size: int = 40):
+    """
+    Print do cabeçalho do menu.
+
+    title: Título do menu.
+
+    size: Largura máxima do menu.
+    """
+    print("+" + "-"*size + "+")
+    print("|" + f" {title} ".center(size) + "|")
+    print("+" + "-"*size + "+")
+    return
 
 def main_menu():
-    show_main_menu()
-    main_menu_selection()
+    """Acoplamento para as funções do menu principal"""
+
+    isRunning = True
+
+    while isRunning:
+        show_main_menu()
+        select = main_menu_selection()
+
+        if select == 0:
+            print("Encerrando programa...")
+            isRunning = False
+        elif select == 1:
+            statistics_menu()
+        elif select == 2:
+            pass
+        elif select == 3:
+            pass
+    return
 
 def show_main_menu():
     """
-    Função para mostrar o menu principal
+    Função para printar o menu principal
     """
     size: int = 40 # largura maxima
-    print("+" + "-"*size + "+")
-    print("|" + " Menu principal ".center(size) + "|")
-    print("+" + "-"*size + "+")
+    print_header("Menu principal", size)
     options = [
+        "Sair",
         "Estatísticas de manchas solares",
         "Gráficos e visualizações",
         "Análise de ciclos solares",
     ]
-    for i in range(len(options)):
-        print(f"| {i+1}. {options[i]:<{size-4}}|")
+    for i, option in enumerate(options):
+        print(f"| {i}. {option:<{size-4}}|")
     print("+" + "-"*size + "+")
     return
 
-def main_menu_selection():
+def main_menu_selection() -> int:
     """
-    Função para selecionar a opção certa do Menu Principal
-    """
-    selecting = True
+    Função para selecionar as opções do Menu Principal.
 
-    while selecting:
+    Retorno: Opção selecionada.
+    """
+    isSelecting = True
+
+    while isSelecting:
         try:
-            select = int(input("Sua opção: "))
+            option = int(input("Sua opção: "))
 
-            if select < 1 or select > 3:
+            if option not in range(0,4):
                 raise ValueError
             
-            selecting = False  
+            isSelecting = False  
         except ValueError:
-            print("Por favor digite um número de 1 a 3!")
+            print("Por favor digite um número de 0 a 3!")
+    
+    return option
 
-    if select == 1:
-        pass
-    elif select == 2:
-        pass
-    elif select == 3:
-        pass
+def statistics_menu():
+    """Acoplamento e loop do menu de estatísticas."""
+    option = -1
+    while option != 0:
+        show_statistics_menu()
+        option = statistics_menu_selection()
+
+        if option == 0:
+            print("Voltando ao Menu Principal...\n")
+        elif option == 1:
+            pass
+        elif option == 2:
+            pass
+        elif option == 3:
+            pass
+        elif option == 4:
+            pass
     return
+
+def show_statistics_menu():
+    """
+    Função para printar o menu de Estatisticas.
+    """
+    
+    size: int = 40 # Largura máxima
+
+    options = [
+        "Voltar",
+        "Dias sem manchas em um mês",
+        "Mês com mais manchas em um intervalo",
+        "Média mensal de manchas",
+        "Desvio padrão mensal",
+    ]
+    print_header("Menu de Estatisticas", size)
+
+    for i, option in enumerate(options):
+        print(f"| {i}. {option:<{size-4}}|")
+
+    print("+" + "-"*size + "+")
+    return
+
+def statistics_menu_selection() -> int:
+    """
+    Função para selecionar as opções do Menu de Estatisticas.
+
+    Retorno: Opção selecionada.
+    """
+    isSelecting = True
+    while isSelecting:
+        try:
+            option = int(input("Sua opção: "))
+
+            if option not in range(5):
+                raise ValueError
+            
+            isSelecting = False
+
+        except ValueError:
+            print("Por favor digite um número de 0 a 4!")
+    return option
